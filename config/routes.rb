39eca88353302134920a7ca66746cf82b02d44f4
Chris Rodrigues <crod951@gmail.com>
2019-08-03 19:00:04 -0700

    Added functionality to update metric values for a user then redirect back to metric's user list page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   get "metrics/:metric_id", to: 'metrics#show'
   get "users/:user_id/:metric_id/:value_id", to: 'users#edit'
-
+  put "users/:user_id/update", to: 'users#update'
 end
